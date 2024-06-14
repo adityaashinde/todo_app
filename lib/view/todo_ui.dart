2bd2b2407caa2edc136a921473e4d931436f49de
adityaashinde <adityashinde4580@gmail.com>
2024-06-14 21:39:20 +0530

    todoUI update

--- lib/view/todo_ui.dart
+++ lib/view/todo_ui.dart
@@ -1,5 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoAppUI extends StatefulWidget {
   const TodoAppUI({super.key});
@@ -12,26 +19,231 @@ class _TodoAppState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 35),
-            child: Text(
-              "Good Morning",
-              style: GoogleFonts.quicksand(
-                color: Colors.white,
-                fontWeight: FontWeight.w400,
-                fontSize: 22,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 100,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 35),
+              child: Text(
+                "Good Morning",
+                style: GoogleFonts.quicksand(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 22,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 35),
+              child: Text(
+                "Aditya",
+                style: GoogleFonts.quicksand(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(217, 217, 217, 1),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "CREATE TASKS",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 20),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+                          ),
+                        ),
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return Slidable(
+                              closeOnScroll: true,
+                              endActionPane: ActionPane(
+                                extentRatio: 0.2,
+                                motion: const DrawerMotion(),
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        GestureDetector(
+                                          child: Container(
+                                            padding: const EdgeInsets.all(10),
+                                            height: 40,
+                                            width: 40,
+                                            decoration: BoxDecoration(
+                                              color: const Color.fromRGBO(
+                                                  89, 57, 241, 1),
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                            child: const Icon(
+                                              Icons.edit,
+                                              color: Colors.white,
+                                              size: 20,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 20,
+                                        ),
+                                        GestureDetector(
+                                          child: Container(
+                                            padding: const EdgeInsets.all(5),
+                                            height: 40,
+                                            width: 40,
+                                            decoration: BoxDecoration(
+                                              color: const Color.fromRGBO(
+                                                  89, 57, 241, 1),
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                            child: const Icon(
+                                              Icons.delete,
+                                              color: Colors.white,
+                                              size: 20,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              child: Container(
+                                margin: const EdgeInsets.only(top: 10),
+                                padding: const EdgeInsets.only(
+                                  left: 20,
+                                  top: 20,
+                                  bottom: 20,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: const Color.fromRGBO(255, 255, 255, 1),
+                                  border: Border.all(
+                                      color:
+                                          const Color.fromRGBO(0, 0, 0, 0.05),
+                                      width: 0.5),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      offset: Offset(0, 4),
+                                      blurRadius: 20,
+                                      color: Color.fromRGBO(0, 0, 0, 0.13),
+                                    ),
+                                  ],
+                                  borderRadius:
+                                      const BorderRadius.all(Radius.zero),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 60,
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Color.fromRGBO(
+                                                217, 217, 217, 1),
+                                          ),
+                                          child: Image.network("photo"),
+                                        ),
+                                        const SizedBox(
+                                          width: 20,
+                                        ),
+                                        SizedBox(
+                                          width: 260,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Lorem Ispum is simply dummy industry",
+                                                style: GoogleFonts.inter(
+                                                  fontWeight: FontWeight.w500,
+                                                  fontSize: 15,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Text(
+                                                "Simply dummy text of the printing and type setting industry. Lorem Ipsum Lorem Ipsum Lorem",
+                                                style: GoogleFonts.inter(
+                                                  color: const Color.fromRGBO(
+                                                      0, 0, 0, 0.7),
+                                                  fontWeight: FontWeight.w400,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
